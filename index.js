33a3257e790ce4d27e544c83d78796ee978a30d6
Oganesman <oganespetikyan@gmail.com>
2022-07-30 22:20:58 +0300

    init

--- index.js
+++ index.js
@@ -12,29 +12,29 @@ app.use(bodyParser.json(), cors())
 app.options('*', cors())
 
 app.post('/', (req, res) => {
+	const iat = Math.round(new Date().getTime() / 1000) - 30;
+	const exp = iat + 60 * 60 * 2
 
-  const iat = Math.round(new Date().getTime() / 1000) - 30;
-  const exp = iat + 60 * 60 * 2
+	const oHeader = { alg: 'HS256', typ: 'JWT' }
 
-  const oHeader = { alg: 'HS256', typ: 'JWT' }
+	const oPayload = {
+		sdkKey: process.env.ZOOM_SDK_KEY,
+		mn: req.body.meetingNumber,
+		role: req.body.role,
+		iat: iat,
+		exp: exp,
+		appKey: process.env.ZOOM_SDK_KEY,
+		tokenExp: iat + 60 * 60 * 2
+	}
 
-  const oPayload = {
-    sdkKey: process.env.ZOOM_SDK_KEY,
-    mn: req.body.meetingNumber,
-    role: req.body.role,
-    iat: iat,
-    exp: exp,
-    appKey: process.env.ZOOM_SDK_KEY,
-    tokenExp: iat + 60 * 60 * 2
-  }
+	const sHeader = JSON.stringify(oHeader)
+	const sPayload = JSON.stringify(oPayload)
+	const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_SDK_SECRET)
 
-  const sHeader = JSON.stringify(oHeader)
-  const sPayload = JSON.stringify(oPayload)
-  const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_SDK_SECRET)
-
-  res.json({
-    signature: signature
-  })
+	res.json({
+		signature: signature
+	})
 })
+
 
 app.listen(port, () => console.log(`Zoom Meeting SDK Sample Signature Node.js on port ${port}!`))
